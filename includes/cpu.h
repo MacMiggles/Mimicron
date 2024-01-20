4f8c4759f2c36f395cb2d2996c13d22331fa4bf0
MacMiggles <65370033+MacMiggles@users.noreply.github.com>
2024-01-20 01:15:23 +0000

    main

--- includes/cpu.h
+++ includes/cpu.h
@@ -8,21 +8,30 @@ typedef struct CPU
    using Byte = unsigned char;
    using Word = unsigned short;
 
-   Word PC, SP;
    Byte A, B, C, D, E, H, L;
+   Word PC, SP; //16bit
    // register. A is accumulator, rest is "scatchpad" 
 
+   // A, B, D, H 15 ... 8
+   // F(Flag), c,E, L 7...0
+
+   // flag 7:S,  6:Z  ,5:0 ,  4:A ,  3:0 ,  2:P ,  1:1 ,  0:c 
    Byte S : 1; // sign flag
    Byte Z : 1; // zero flag 
+   Byte f : 1; 
+   Byte A : 1; // aux carry
+   Byte f2 : 1;
    Byte P : 1; // parity flag 
+   Byte f3 : 1;
    Byte C : 1; // carry flag 
-   Byte H : 1; // aux carry
+   
    struct BUS bus;
 }CPU;
 
-void init(struct CPU * cpu);
-unsigned int fetch(struct CPU * cpu);
-int execute(struct CPU * cpu, unsigned int inst);
-void dump_registers(struct CPU *cpu);
+void cpu_init(struct CPU * cpu);
+void cpu_step(struct CPU * cpu);
+unsigned int cpu_fetch(struct CPU * cpu);
+int cpu_execute(struct CPU * cpu, unsigned int inst);
+void cpu_dump_registers(struct CPU *cpu);
 
 #endif
--- src/bus.cpp
+++ src/bus.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "../includes/bus.h"
 
 unsigned int bus_load(BUS* bus, unsigned int addr, unsigned int size){
